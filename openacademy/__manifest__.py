cb35b7215ac8d9f997062e4f86b3ccb43ea07e3d
jmateum07 <john.toolkt@gmail.com>
2020-10-05 17:40:23 +0000

    test

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -1,25 +1,22 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "openacademy",
+    'name': "Open Academy",
 
-    'summary': """
-         Open Academy module for managing trainings:
+    'summary': """Manage trainings""",
+
+    'description': """
+        Open Academy module for managing trainings:
             - training courses
             - training sessions
             - attendees registration
-""",
-
-    'description': """
-        Long description of module's purpose
     """,
 
-    'author': "Toolkit",
+    'author': "Toolkt",
     'website': "http://www.toolkt.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Test',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -28,12 +25,11 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'templates.xml',
         'views/openacademy.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
-        'demo/demo.xml',
+        'demo.xml',
     ],
 }
